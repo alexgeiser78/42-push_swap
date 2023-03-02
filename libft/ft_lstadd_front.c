129551457db19b7f9a4e717ccc325e7746ab5fd5
Pleiades <113288425+alexgeiser78@users.noreply.github.com>
2023-03-02 13:59:32 +0100

    Add files via upload
    02.03.23

---
+++ libft/ft_lstadd_front.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ageiser <ageiser@student.42barcelo>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/05 13:14:50 by ageiser           #+#    #+#             */
+/*   Updated: 2022/10/06 12:07:24 by ageiser          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (lst)
+	{
+		if (*lst)
+			new->next = *lst;
+		*lst = new;
+	}
+}
+//ligne 15 fonction qui ajoute un maillon dans la liste
+//ligne 20 new qui pointe next devien un pointeur 
+//ligne 21 le pointeur pointe sur l'element cree
